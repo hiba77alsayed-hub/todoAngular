5e0522899712583ddb1a7377f3d6d0334fb05c89
hiba77alsayed-hub <hiba77alsayed@gmail.com>
2026-03-28 11:19:53 +0300

    .

--- src/app/pages/task-list/task-list.ts
+++ src/app/pages/task-list/task-list.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { TaskService } from '../../services/task-service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
